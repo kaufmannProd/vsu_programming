ac4312aa75dd5bc30fa1c4b31420d50fed497a3f
kaufmannProd <55966310+kaufmannProd@users.noreply.github.com>
2019-09-30 08:07:38 +0300

    Create 4.py

---
+++ HW_2/4.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def u_Brackets(u_Str):
+	
+	# считаем скобочку
+	BR_open = u_Str.count('(')
+	BR_close = u_Str.count(')')
+	
+	# сколько не хватает?
+	BR_dif = abs(BR_open - BR_close)
+	
+	# сморим каких не хватает
+	if BR_open > BR_close:
+		print("Не хватает {BR} - {SH} шт.".format(BR = ")", SH = BR_dif))
+		return 0
+	elif BR_open < BR_close:
+		print("Не хватает {BR} - {SH} шт.".format(BR = "(", SH = BR_dif))
+		return 0
+	return "Всего хватает, красава!"
+
+u_Str = input("Ну давай, пиши че ты там хотел. Скобок хватает? ")
+u_Brackets(u_Str)
