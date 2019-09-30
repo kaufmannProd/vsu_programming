8903f13b9bc6bf702c0d235db5713c2d828ceb09
kaufmannProd <55966310+kaufmannProd@users.noreply.github.com>
2019-09-30 05:16:31 +0300

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# шуршим циферками
+a = float(input("Введите a: "))
+b = float(input("Введите b: "))
+c = float(input("Введите c: "))
+d = float(input("Введите d: "))
+f = float(input("Введите f: "))
+
+# вычисляем вычисления
+if f - d:
+	print("(a * b - c) / (f - d) = ", (a * b - c) / (f - d))
+else:
+	print("Делить на ноль нельзя!")
