a3b35fa3f3d93c110747de862c3f7618dec1a27b
kaufmannProd <55966310+kaufmannProd@users.noreply.github.com>
2019-09-30 08:08:09 +0300

    Create 6.py

---
+++ HW_2/6.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+
+# Ввод строчки и искомой цифры
+u_str = input("Введи строчку: ")
+num = int(input("Какую цифру забыл?: "))
+
+# достаем все циферки из строки
+search_num = re.findall("\d", u_str)
+
+print("Держи свою цифру - {}, Дурак!".format(search_num[num - 1]))
