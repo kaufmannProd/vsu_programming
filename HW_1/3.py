9a710abac9a87e82066485239b1b7c0845d3968d
kaufmannProd <55966310+kaufmannProd@users.noreply.github.com>
2019-09-30 11:59:10 +0300

    Update 3.py

--- HW_1/3.py
+++ HW_1/3.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from math import sqrt
+
 # шуршим циферками
 x = float(input("Введите x: "))
 y = float(input("Введите y: "))
 z = float(input("Введите z: "))
 
 # вывод результата выражения, округленного до 2-х знаков после запятой
-print("Длина вектора = {:.2f}".format((x**2 + y**2 +z**2)**.5))
+print("Длина вектора = {:.2f}".format(sqrt(x**2 + y**2 +z**2)))
