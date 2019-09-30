de27d0fe688f03e17710921d2398345f7f507a0d
kaufmannProd <55966310+kaufmannProd@users.noreply.github.com>
2019-09-30 08:07:22 +0300

    Create 3.py

---
+++ HW_2/3.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# построение треугольника
+def build_TR():
+	
+	# длина стороны?
+	dlin = int(input("Введите длину стороны треугольника: "))
+	
+	# рисуем
+	for i in range(dlin):
+		print(" " * (dlin - i) + "*" * (i + 1) + "*" * i)
+
+build_TR()
+	
+
