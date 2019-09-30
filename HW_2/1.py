192dc79b8b6de82eaed44ed4facecbb5e6ad2b19
kaufmannProd <55966310+kaufmannProd@users.noreply.github.com>
2019-09-30 15:08:27 +0300

    Update 1.py

--- HW_2/1.py
+++ HW_2/1.py
@@ -7,7 +7,7 @@ str_OK = input("Введи че-нибудь приличное: ")
 
 # обкашляем че он там написал и пихаем в массив
 while str_OK:
-	mas_N.append(str_OK)
-	str_OK = input("Введи че-нибудь приличное: ")
+    mas_N.append(str_OK)
+    str_OK = input("Введи че-нибудь приличное: ")
 	
 print("Вот че ты наделал:\n", mas_N)
