9d3cf40f41b4ee3c7c7efba3b6cfdb4732b2948d
kaufmannProd <55966310+kaufmannProd@users.noreply.github.com>
2019-09-30 05:17:40 +0300

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# флот коорд
+x = float(input("Введите координату x: "))
+y = float(input("Введите координату y: "))
+
+# в какой четверти
+if x > 0 and y > 0:
+	print("1-я четверть")
+elif x < 0 and y > 0:
+	print("2-я четверть")
+elif x < 0 and y < 0:
+	print("3-я четверть")
+elif x > 0 and y < 0:
+	print("4-я четверть")
+elif not x and not y:
+	print("Точка на оси -|-")
