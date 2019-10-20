8a3d0ce6cef938dba37f50c0ed6b3f37e0c13e71
kaufmannProd <55966310+kaufmannProd@users.noreply.github.com>
2019-10-20 17:56:15 +0300

    Create shell.py

---
+++ HW_4/shell.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def shellSort(mas):
+    rast = len(mas) // 2
+
+    while rast > 0:
+        for startPos in range(rast):
+            sortValue(mas, startPos, rast)
+
+        print("После инкрементации размера на", rast,"массив:", mas)
+
+        rast //= 2
+
+def sortValue(mas, low, rast):
+    
+    for x in range(low + rast, len(mas), rast):
+        currentvalue = mas[x]
+        pos = x
+
+        while pos >= rast and mas[pos - rast] > currentvalue:
+            mas[pos] = mas[pos - rast]
+            pos = pos - rast
+
+        mas[pos] = currentvalue
+
+from random import randint
+
+is_mas = []
+
+for x in range(20):
+    is_mas.append(randint(0,100))
+
+print(f"Исходный массив:\n{is_mas[::1]}")
+
+shellSort(is_mas)
