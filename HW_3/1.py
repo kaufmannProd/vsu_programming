1b86229f77f8a416dd061b8d2326adebc5edb9dd
kaufmannProd <55966310+kaufmannProd@users.noreply.github.com>
2019-10-20 17:54:53 +0300

    Create 1.py

---
+++ HW_3/1.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+u_str = "".join(input("Давай пример, порешаем: "))
+symb = {"+", "-", "*", "**", "/", "//", "%"}
+op = "".join(symb & set(u_str))
+ind_op = u_str.find(op)
+a = u_str[:ind_op]
+b = u_str[ind_op + 1:]
+
+if b.find(op) != -1:
+    op *= 2
+    b = u_str[ind_op + 2:]
+
+
+a = float(a)
+b = float(b)
+symb = {"+": a + b, "-": a - b, "*": a * b, "**": a ** b, "/": a / b, "//": a // b, "%": a % b}
+print(f"Вот че получилось: {symb[op]}")
